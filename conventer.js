b72cde20bc7270ceedee0a2d8f8427eeda7dfbd6
sonamirijanyan <57571008+sonamirijanyan@users.noreply.github.com>
2019-11-19 19:18:37 +0400

    finished

---
+++ conventer.js
@@ -0,0 +1,41 @@
+function convertToBinary(number) {
+    let array = [];
+
+    for (let i = number; i > 0; i = Math.floor(i / 2)){
+        array.push(i % 2);
+    }
+
+    let result = '';
+    for (let j = array.length - 1; j >= 0; j--) {
+        result += array[j];
+    }
+    return result;
+}
+
+function convertToHexa(number) {
+
+    if (number < 0) {
+        number += 0xFFFFFFFF + 1;
+    }
+
+    let array = [];
+    let values = ["A", "B", "C", "D", "E", "F"];
+
+    for (let i = number; i > 0; i = Math.floor(i / 16)){
+        if(i % 16 >= 10) {
+            array.push(values[i % 16 - 10]);
+        }else {
+            array.push(i % 16);
+    }
+
+    let result = '';
+    for (let j = array.length - 1; j >= 0; j--) {
+        result += array[j];
+    }
+    return result;
+}
+
+module.exports = {
+    convertToHexa,
+    convertToBinary
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+const reader = require('readline-sync');
+const utils = require('./converter');
+
+const number = reader.questionInt("Please enter the number you want to convert = ");
+const choice = reader.questionInt("Please enter choice: \n 1:Binary \n 2:Hexadecimal \n");
+
+if(choice === 1){
+    console.log(utils.convertToBinary(number));
+}else if(choice === 2){
+    console.log(utils.convertToHexa(number));
+}else if(typeof choice === "number"){
+    console.log("There is no choice for that number! ");
+}else{
+    console.log("Imported value should be number");
+} 
